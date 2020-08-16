7bce2204fb56f837bc6836a53159ea73a185e716
Meitar Basson <meitar.startup@gmail.com>
2020-08-16 06:17:57 +0300

    New project was created

---
+++ Bookly/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bookly
+//
+//  Created by Meitar Basson on 16/08/2020.
+//  Copyright © 2020 Meitar Basson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
